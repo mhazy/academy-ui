b1b17360d311e63682e8e32cc64752b630ab5fd5
Mark Hayes <mark.hayes@rangle.io>
2017-04-30 12:51:12 -0400

    fix tests for components with css

--- src/hello/hello.test.tsx
+++ src/hello/hello.test.tsx
@@ -7,6 +7,6 @@ import { Hello } from "./hello";
 describe("Component: Hello", () => {
   it("should render without throwing an error", () => {
     const wrapper = shallow(<Hello greeting="Hello" />);
-    expect(wrapper.contains(<p>Hello Component</p>)).toBe(true);
+    expect(wrapper.contains(<p className="hello">Hello Component</p>)).toBe(true);
   });
 });
